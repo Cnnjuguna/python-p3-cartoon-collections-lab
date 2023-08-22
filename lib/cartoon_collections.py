7bf65cf25c28dc7aef9d278e9258b19c274cbbfd
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-08-22 05:46:27 +0300

    Initial commit

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,17 @@
-def roll_call_dwarves():
+def roll_call_dwarves(dwarves=["Doc", "Dopey", "Bashful", "Grumpy"]):
+    for dwarf in dwarves:
+        print(dwarf)
     pass
 
-def summon_captain_planet():
+
+def summon_captain_planet(planeteer_calls=["earth", "wind", "fire", "water", "heart"]):
+    return [f"{a_call.title()}!" for a_call in planeteer_calls]
     pass
+
 
 def long_planeteer_calls():
     pass
+
 
 def find_the_cheese():
     pass
