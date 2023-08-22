07eb8556970054440e1ef18e6473814437eb2328
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-08-22 06:41:56 +0300

    Complete assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves=["Doc", "Dopey", "Bashful", "Grumpy"]):
-    for dwarf in dwarves:
-        print(dwarf)
+    for index, dwarf in enumerate(dwarves, start=1):
+        print(f"{index}. {dwarf}")
     pass
 
 
@@ -9,9 +9,26 @@ def summon_captain_planet(planeteer_calls=["earth", "wind", "fire", "water", "he
     pass
 
 
-def long_planeteer_calls():
+def long_planeteer_calls(assorted_words=["two", "go", "industrious", "bop"]):
+    return any(len(a_call) > 4 for a_call in assorted_words)
     pass
 
 
-def find_the_cheese():
+def find_the_cheese(soups=["tomato soup", "cheddar", "oyster crackers", "gouda"]):
+    # for item in soups:
+    #     if item == "cheddar" or item == "gouda" or item == "camembert":
+    #         return item
+
+    cheese_options = {"cheddar", "gouda", "camembert"}
+    common_cheeses = cheese_options & set(soups)
+    if common_cheeses:
+        return common_cheeses.pop()
+    else:
+        return None
+
+    # cheese_options = {"cheddar", "gouda", "camembert"}
+    # for item in soups:
+    #     if item in cheese_options:
+    #         return item
+
     pass
